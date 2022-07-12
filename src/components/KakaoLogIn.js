dbe09984fd3e66b2042a05d61755ea2af5ebfe9e
DrInnovatio <turing1723@gmail.com>
2022-07-12 19:01:37 +0900

    tried to install kakao login again.

--- src/components/KakaoLogIn.js
+++ src/components/KakaoLogIn.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const KakaoLogIn = () => {
 
+const KakaoLogIn = () => {
 
   return (
     <View>
-      <script src='https://developers.kakao.com/sdk/js/kakao.js'></script>
-      
-      
+        <a id="custom-login-btn" href="javascript:loginWithKakao()">
+          <img
+            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+            width="222"
+            alt="카카오 로그인 버튼"
+          />
+        </a>
+
+        <p id="token-result"></p>
     </View>
-  );
+    
+  )
 };
 
-const styles = StyleSheet.create({});
 
-export default KakaoLogIn;
+
+
 
 //  64db95e8d66057d4911cae341a72ed32
 
 //https://developers.kakao.com/sdk/js/kakao.js
+
