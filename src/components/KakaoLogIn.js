18291f08e902008988e20cfeeeca44074ed44000
DrInnovatio <turing1723@gmail.com>
2022-07-13 18:48:14 +0900

    tried to install kakao login.

--- src/components/KakaoLogIn.js
+++ src/components/KakaoLogIn.js
@@ -4,22 +4,52 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 const KakaoLogIn = () => {
 
+   const loginWithKakao = () => {
+    Kakao.Auth.authorize({
+      redirectUri: 'https://developers.kakao.com/tool/demo/oauth'
+    })
+  }
+  // 아래는 데모를 위한 UI 코드입니다.
+  displayToken()
+  
+  const displayToken = () => {
+    const token = getCookie('authorize-access-token')
+    if(token) {
+      Kakao.Auth.setAccessToken(token)
+      Kakao.Auth.getStatusInfo(({ status }) => {
+        if(status === 'connected') {
+          document.getElementById('token-result').innerText = 'login success. token: ' + Kakao.Auth.getAccessToken()
+        } else {
+          Kakao.Auth.setAccessToken(null)
+        }
+      })
+    }
+  }
+  const getCookie = (name) => {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
+
   return (
     <View>
-        <a id="custom-login-btn" href="javascript:loginWithKakao()">
+        <Text id="custom-login-btn" href="javascript:loginWithKakao()">
           <img
             src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
             width="222"
             alt="카카오 로그인 버튼"
           />
-        </a>
+        </Text>
 
-        <p id="token-result"></p>
+        <Text id="token-result"></Text>
     </View>
     
   )
 };
 
+
+export default KakaoLogIn;
 
 
 
